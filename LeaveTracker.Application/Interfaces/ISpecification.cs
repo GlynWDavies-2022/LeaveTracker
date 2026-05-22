2996ddb0a986b99288b08c43209b472e0ed84ca0
Glyn Davies <glynwdavies@gmail.com>
2026-05-22 07:21:05 +0100

    Removed generic repository and specification pattern.

--- LeaveTracker.Application/Interfaces/ISpecification.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Linq.Expressions;
-
-namespace LeaveTracker.Application.Interfaces;
-
-public interface ISpecification<T>
-{
-    public Expression<Func<T,bool>> Criteria { get; }
-}
--- LeaveTracker.Application/Specifications/BaseSpecification.cs
+++
@@ -1,9 +0,0 @@
-﻿using LeaveTracker.Application.Interfaces;
-using System.Linq.Expressions;
-
-namespace LeaveTracker.Application.Specifications;
-
-public class BaseSpecification<T>(Expression<Func<T,bool>> criteria) : ISpecification<T>
-{
-    public Expression<Func<T,bool>> Criteria => throw new NotImplementedException();
-}
