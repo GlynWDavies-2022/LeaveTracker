4fb73cb7bb4329bc5a842ddbfd538f515d2377a0
Glyn Davies <glynwdavies@gmail.com>
2026-05-22 06:52:53 +0100

    Added BaseSpecification.

---
+++ LeaveTracker.Application/Specifications/BaseSpecification.cs
@@ -0,0 +1,9 @@
+﻿using LeaveTracker.Application.Interfaces;
+using System.Linq.Expressions;
+
+namespace LeaveTracker.Application.Specifications;
+
+public class BaseSpecification<T>(Expression<Func<T,bool>> criteria) : ISpecification<T>
+{
+    public Expression<Func<T,bool>> Criteria => throw new NotImplementedException();
+}
