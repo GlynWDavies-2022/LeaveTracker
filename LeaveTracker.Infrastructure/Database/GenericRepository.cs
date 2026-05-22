b24c7adb3b28df14599ef440ed00a4ec2b51a9c6
Glyn Davies <glynwdavies@gmail.com>
2026-05-22 05:32:36 +0100

    Added Generic Repository.

---
+++ LeaveTracker.Infrastructure/Database/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using LeaveTracker.Application.Interfaces;
+using LeaveTracker.Domain.Entities;
+
+namespace LeaveTracker.Infrastructure.Database;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    public void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Exists(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
