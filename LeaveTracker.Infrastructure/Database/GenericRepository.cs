f01e0048d162a7f1ce990bb024ccd5e308e03454
Glyn Davies <glynwdavies@gmail.com>
2026-05-22 05:48:25 +0100

    Added DbContext.

--- LeaveTracker.Infrastructure/Database/GenericRepository.cs
+++ LeaveTracker.Infrastructure/Database/GenericRepository.cs
@@ -1,42 +1,46 @@
 ﻿using LeaveTracker.Application.Interfaces;
 using LeaveTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace LeaveTracker.Infrastructure.Database;
 
-public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+public class GenericRepository<T>(LeaveTrackerContext context) : IGenericRepository<T> where T : BaseEntity
 {
     public void Add(T entity)
     {
-        throw new NotImplementedException();
+        context.Set<T>().Add(entity);
     }
 
-    public bool Exists(int id)
+    public async Task<T?> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await context.Set<T>().FindAsync(id);
     }
 
-    public Task<T?> GetByIdAsync(int id)
+    public async Task<IReadOnlyList<T>> ListAllAsync()
     {
-        throw new NotImplementedException();
+        return await context.Set<T>().ToListAsync();
     }
 
-    public Task<IReadOnlyList<T>> ListAllAsync()
+
+    public void Update(T entity)
     {
-        throw new NotImplementedException();
+        context.Set<T>().Attach(entity);
+
+        context.Entry(entity).State = EntityState.Modified;
     }
 
     public void Remove(T entity)
     {
-        throw new NotImplementedException();
+        context.Set<T>().Remove(entity);
     }
 
-    public Task<bool> SaveAllAsync()
+    public async Task<bool> SaveAllAsync()
     {
-        throw new NotImplementedException();
+        return await context.SaveChangesAsync() > 0;
     }
 
-    public void Update(T entity)
+    public bool Exists(int id)
     {
-        throw new NotImplementedException();
+        return context.Set<T>().Any(entity => entity.Id == id);
     }
 }
---
+++ LeaveTracker.Infrastructure/Database/LeaveTrackerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LeaveTracker.Infrastructure.Database;
+
+public class LeaveTrackerContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
