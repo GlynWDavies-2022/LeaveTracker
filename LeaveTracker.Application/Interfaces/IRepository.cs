737420ba88c1ffc3d7d6e66e25fd789e894e0d45
Glyn Davies <glynwdavies@gmail.com>
2026-06-04 19:22:22 +0100

    Completed generic repository.

--- LeaveTracker.Application/Interfaces/IRepository.cs
+++ LeaveTracker.Application/Interfaces/IRepository.cs
@@ -1,8 +1,16 @@
 ﻿using LeaveTracker.Domain.Entities;
+using System.Linq.Expressions;
 
 namespace LeaveTracker.Application.Interfaces;
 
 public interface IRepository<TEntity> where TEntity : BaseEntity
 {
+    public Task CreateAsync(TEntity entity);
     public Task<IEnumerable<TEntity>> GetAllAsync();
+    public Task<TEntity?> GetByIdAsync(int id);
+    public Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity,bool>> predicate);
+    public void Update(TEntity entity);
+    public void Delete(TEntity entity);
+
+    public Task<int> SaveChangesAsync();
 }
--- LeaveTracker.Infrastructure/Persistence/Repository.cs
+++ LeaveTracker.Infrastructure/Persistence/Repository.cs
@@ -2,6 +2,7 @@
 using LeaveTracker.Domain.Entities;
 using LeaveTracker.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace LeaveTracker.Infrastructure.Persistence;
 
@@ -18,8 +19,42 @@ public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEnti
         _dbSet = _dbContext.Set<TEntity>();
     }
 
+    public virtual async Task CreateAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+
+        await _dbContext.SaveChangesAsync();
+    }
     public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
     {
         return await _dbSet.ToListAsync();
+    }
+    public virtual async Task<TEntity?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+    public virtual void Update(TEntity entity)
+    {
+        _dbSet.Update(entity);
+
+        _dbContext.SaveChanges();
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task<int> SaveChangesAsync()
+    {
+        return await _dbContext.SaveChangesAsync();
+    }
+
+
+    public virtual void Delete(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+
+        _dbContext.SaveChanges();
     }
 }
