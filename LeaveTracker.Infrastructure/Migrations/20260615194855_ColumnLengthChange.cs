13d16099a8bbc6100bf69f90bb2f3d68ae898c33
Glyn Davies <glynwdavies@gmail.com>
2026-06-15 21:03:40 +0100

    Adjusted user configuration.

---
+++ LeaveTracker.Infrastructure/Migrations/20260615194855_ColumnLengthChange.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnLengthChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "VARBINARY(MAX)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "VARBINARY(64)");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "VARBINARY(MAX)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "VARBINARY(64)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "VARBINARY(64)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "VARBINARY(MAX)");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "VARBINARY(64)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "VARBINARY(MAX)");
+        }
+    }
+}
