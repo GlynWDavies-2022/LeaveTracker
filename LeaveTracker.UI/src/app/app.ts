717043aab51c93a529a369614aadf7525156e3bf
Glyn Davies <glynwdavies@gmail.com>
2026-06-16 12:54:42 +0100

    Completed app-header component.

--- LeaveTracker.UI/src/app/app.ts
+++ LeaveTracker.UI/src/app/app.ts
@@ -1,11 +1,40 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AppHeader } from '../app/shared/components/app-header/app-header';
+import { AccountService } from './features/account/services/account-service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  imports: [AppHeader, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('LeaveTracker.UI');
+export class App implements OnInit {
+
+  private accountService = inject(AccountService);
+
+  protected router = inject(Router);
+
+  private http = inject(HttpClient);
+
+  protected readonly title = 'Leave Tracker';
+
+  ngOnInit(): void {
+
+    this.setCurrentUser();
+
+  }
+
+  setCurrentUser() {
+
+    const userString = localStorage.getItem('user');
+
+    if (!userString) return;
+
+    const user = JSON.parse(userString);
+
+    this.accountService.currentUser.set(user);
+  }
+
 }
--- LeaveTracker.UI/src/app/shared/components/app-header/app-header.ts
+++ LeaveTracker.UI/src/app/shared/components/app-header/app-header.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AccountService } from '../../../features/account/services/account-service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule,RouterLink,RouterLinkActive],
   templateUrl: './app-header.html',
   styleUrl: './app-header.css',
 })
 
-export class AppHeader {}
+export class AppHeader {
+
+  private router = inject(Router);
+
+  protected accountService = inject(AccountService);
+
+  protected credentials: any = {};
+
+  login(): void {
+
+    this.accountService.login(this.credentials).subscribe({
+      next: result => {
+        this.router.navigateByUrl('/');
+        this.credentials = {};
+      },
+      error: error => console.log(error)
+    });
+
+  }
+
+  logout(): void {
+    this.accountService.logout();
+    this.router.navigateByUrl('/login');
+  }
+
+}
